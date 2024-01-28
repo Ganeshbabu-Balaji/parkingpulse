ebfceda44c42d80ba8e9fd834bbb0f4fd913fe81
Balaji Ganeshbabu <33739049+Ganeshbabu-Balaji@users.noreply.github.com>
2024-01-27 19:43:20 -0500

    Add files via upload

---
+++ emailHandler.py
@@ -0,0 +1,46 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+
+# create SMTP session
+s = smtplib.SMTP('smtp.gmail.com', 587)
+# start TLS for security
+s.starttls()
+print("Starting Server")
+
+# Authentication
+s.login("parkingpulseapp@gmail.com", "pslaydwoghpgbkrn")
+
+# Create a MIMEMultipart object for the message
+msg = MIMEMultipart()
+
+# Add subject to the email
+msg['Subject'] = "Important: PACE Officer Detected"
+
+# Create the HTML body with an embedded image
+html_body = """
+<html>
+  <body>
+    <p>A PACE officer has been detected near your vehicle.</p>
+    <img src="cid:carcopguy">
+  </body>
+</html>
+"""
+
+msg.attach(MIMEText(html_body, 'html'))
+
+# Embed the image directly into the email body
+image_path = './carcopguy.jpeg'
+with open(image_path, 'rb') as image_file:
+    image_data = image_file.read()
+    image = MIMEImage(image_data, name='carcopguy.jpeg')
+    image.add_header('Content-ID', '<carcopguy>')
+    msg.attach(image)
+
+# sending the mail
+s.sendmail("parkingpulseapp@gmail.com", "parkingpulseapp@gmail.com", msg.as_string())
+
+# terminating the session
+s.quit()
+print("Email Sent")
